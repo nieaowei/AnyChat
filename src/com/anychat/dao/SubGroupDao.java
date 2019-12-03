88a7a6fa604d652a54dc2989fe9cca90293cd4cf
nieaowei <nieaowei@gmail.com>
2019-12-03 17:44:24 +0800

    第三次提交。

---
+++ src/com/anychat/dao/SubGroupDao.java
@@ -0,0 +1,20 @@
+/**
+ * create:	Dec 3, 2019
+ * author:	nieaowei
+ */
+package com.anychat.dao;
+
+import com.anychat.model.SubGroupModel;
+
+/**
+ * @author nieaowei
+ *
+ */
+public class SubGroupDao {
+	
+	public boolean findSid(SubGroupModel subGroupModel) {
+		
+		return false;
+	}
+	
+}
---
+++ src/com/anychat/model/SubGroupModel.java
@@ -0,0 +1,65 @@
+/**
+ * create:	Dec 3, 2019
+ * author:	nieaowei
+ */
+package com.anychat.model;
+
+/**
+ * @author nieaowei
+ *
+ */
+public class SubGroupModel {
+	private String sid;
+	private String qq;
+	private String sname;
+	private String created;
+	/**
+	 * @return the sid
+	 */
+	public String getSid() {
+		return sid;
+	}
+	/**
+	 * @param sid the sid to set
+	 */
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	/**
+	 * @return the qq
+	 */
+	public String getQq() {
+		return qq;
+	}
+	/**
+	 * @param qq the qq to set
+	 */
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	/**
+	 * @return the sname
+	 */
+	public String getSname() {
+		return sname;
+	}
+	/**
+	 * @param sname the sname to set
+	 */
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	/**
+	 * @return the created
+	 */
+	public String getCreated() {
+		return created;
+	}
+	/**
+	 * @param created the created to set
+	 */
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	
+}
